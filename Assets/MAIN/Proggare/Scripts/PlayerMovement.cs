962998a0ad88d630832cc8e3a851912f0e152691
TrigoIsHere <eliankorberg@gmail.com>
2024-09-11 23:51:10 +0200

    Commit 1.10
    Goofy movement

--- Assets/MAIN/Proggare/Scripts/PlayerMovement.cs
+++ Assets/MAIN/Proggare/Scripts/PlayerMovement.cs
@@ -5,25 +5,60 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //variables
-    [SerializeField] float movementSpeed = 10f;
-    [SerializeField] float acceleration = 1f;
-    [SerializeField] float jumpForce = 5f;
+    [SerializeField] CharacterController controller;
 
-    CharacterController controller;
+    [Header("Movement Variables")]
+    [SerializeField] float speed = 6f;
+    [SerializeField] float gravity = -9.81f;
+    [SerializeField] float jumpHeight = 1.5f;
 
-    void Awake()
-    {
-        controller = GetComponent<CharacterController>();
-    }
+    [Header("Ground Check")]
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundDistance = 0.4f;
+    [SerializeField] LayerMask groundMask;
+
+    Vector2 moveInput;
+    bool isJumping;
+
+    Vector3 velocity;
+    bool isGrounded;
 
     void Update()
     {
-        
+        //Check if the player is grounded
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        // Get movement direction from the input
+        Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (isJumping && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            isJumping = false; 
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 
-    void OnJump()
+    public void OnMove(InputAction.CallbackContext context)
     {
+        moveInput = context.ReadValue<Vector2>();
+    }
 
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded)
+        {
+            isJumping = true;
+        }
     }
 }
