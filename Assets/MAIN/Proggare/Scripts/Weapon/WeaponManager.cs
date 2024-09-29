720c6056da92e2cb9a0e9f542a48f4ceacf0fd02
Sebastian Larsson Nuñez <sebastian.nunezlarsson@gmail.com>
2024-09-29 17:34:51 +0200

    Commit 1.30.1
    -Computer Switch

--- Assets/MAIN/Proggare/Scripts/Weapon/WeaponManager.cs
+++ Assets/MAIN/Proggare/Scripts/Weapon/WeaponManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 
 //Sebbe
 
@@ -21,6 +22,7 @@ public class WeaponManager : MonoBehaviour
 
     //Private Variabels
     private GameObject antiHierarchySpam;
+    private Vector3 moveDirection;
     private float currentThrowCooldownTime;
     private int currentWeaponInt = 0;
     private int totalWeapons;
@@ -31,21 +33,19 @@ public class WeaponManager : MonoBehaviour
     InputAction fireAction;
     InputAction reloadAction;
     InputAction throwAction;
-
+    InputAction moveAction;
     Transform weaponsParent;
-    PlayerMovement playerMovement;
 
     #region Base Methods
     void Start()
     {
         //Get Cached References
         weaponsParent = GetComponent<Transform>();
-        playerMovement = FindFirstObjectByType<PlayerMovement>();
 
         AntiHierarchySpam();
         UpdateWeaponList();
     }
-    private void Update()
+    private void FixedUpdate()
     {
         if (throwCoolodown && currentThrowCooldownTime > 0) { currentThrowCooldownTime -= Time.deltaTime; }
         else { currentThrowCooldownTime = throwCooldownTime; throwCoolodown = false; }
@@ -56,18 +56,21 @@ public class WeaponManager : MonoBehaviour
         var actionMap = inputActions.FindActionMap("Player");
 
         //Get actions from player ActionMap
+        moveAction = actionMap.FindAction("Move");
         scrollAction = actionMap.FindAction("SwitchWeapon");
         fireAction = actionMap.FindAction("Fire");
         reloadAction = actionMap.FindAction("Reload");
         throwAction = actionMap.FindAction("Throw");
 
         //Enable the action's
+        moveAction.Enable();
         scrollAction.Enable();
         fireAction.Enable();
         reloadAction.Enable();
         throwAction.Enable();
 
         //Subscribe to the performed callback 
+        moveAction.performed += MoveWeaponWithMovement;
         scrollAction.performed += OnSwitchWeapon;
         fireAction.performed += OnFire;
         reloadAction.performed += OnReload;
@@ -182,7 +185,31 @@ public class WeaponManager : MonoBehaviour
     }
     #endregion
 
+    //Working on the name CUNT cuuuuuuuuuuuuuuuhhhhhhhhh :-)
+    private void MoveWeaponWithMovement(InputAction.CallbackContext context)
+    {
+        moveDirection = context.ReadValue<Vector2>();
+        moveDirection.z = moveDirection.y;
+        moveDirection.y = transform.localPosition.y;
+        
+
+        if (moveDirection == Vector3.zero)
+        {
+            transform.localPosition = Vector3.zero;
+        }
+        else
+        {
+            //transform.localPosition = moveDirection;
+
+            //Mathf.Lerp(transform.localPosition.x, transform.localPosition.x + moveDirection.x, 10f);
+            //Mathf.Lerp(transform.localPosition.z, transform.localPosition.z + moveDirection.z, 10f);
+        }
+    }
+
     #region AntiHierarchySpam
+    /// <summary>
+    /// Instansiates AntiHierarchySpam
+    /// </summary>
     public void AntiHierarchySpam()
     {
         GameObject antiSpam = new("AntiHierarchySpam");
