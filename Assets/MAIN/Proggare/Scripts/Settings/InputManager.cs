a1b24e78bfb54136e93327775c0cc54656e3ed33
TrigoIsHere <eliankorberg@gmail.com>
2024-11-21 00:18:57 +0100

    Commit 2.7
    Rebinding fucking suger ass

--- Assets/MAIN/Proggare/Scripts/Settings/InputManager.cs
+++ Assets/MAIN/Proggare/Scripts/Settings/InputManager.cs
@@ -1,8 +1,44 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
 
 public class InputManager : MonoBehaviour
 {
-    //public static 
+    public static Rebinding inputActions;
 
+    private void Awake()
+    {
+        if( inputActions == null) { inputActions = new Rebinding(); }
+    }
 
+    public static void StartRebind(string actionName, int bindingIndex, TextMeshProUGUI statusText)
+    {
+        
+    }
+
+    static void DoRebind(InputAction actionToRebind, int bindingIndex, TextMeshProUGUI statusText)
+    {
+        if (actionToRebind == null || bindingIndex < 0) return;
+
+        statusText.text = $"Press a {actionToRebind.expectedControlType}";
+
+        actionToRebind.Disable();
+
+        var rebind = actionToRebind.PerformInteractiveRebinding(bindingIndex);
+
+        rebind.OnComplete(operation =>
+        {
+            actionToRebind.Enable();
+            operation.Dispose();
+        });
+
+        rebind.OnCancel(operation =>
+        {
+            actionToRebind.Disable();
+            operation.Dispose();
+        });
+
+        //Starts rebinding
+        rebind.Start(); 
+    } 
 }
